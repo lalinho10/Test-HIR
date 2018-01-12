74eb4109324cba24b01200dd6cd55bfc64a4d4e8
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-01-12 15:49:44 -0600

    Se agregan campos al formulario del paso 2 de la emisión del seguro Procura Vida.

--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/procura-vida/procura-vida-p2.component.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/procura-vida/procura-vida-p2.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit }				  from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router }							  from '@angular/router';
 
+import { MODULOS }							  from 'app/core/data/modulos';
+
 @Component({
 	selector: 'pehir-procura-vida-p2',
 	templateUrl: 'procura-vida-p2.component.html'
@@ -10,6 +12,8 @@ import { Router }							  from '@angular/router';
 export class ProcuraVidaP2Component implements OnInit {
 	private frmProcuraVidaP2: FormGroup;
 
+	private modulos = MODULOS;
+
 	constructor(
 		private router: Router,
 		private fb: FormBuilder
@@ -17,7 +21,21 @@ export class ProcuraVidaP2Component implements OnInit {
 
 	ngOnInit() {
 		this.frmProcuraVidaP2 = this.fb.group({
-
+			'agente': ['', Validators.compose([
+				Validators.required
+			])],
+			'modulo': ['', Validators.compose([
+				Validators.required
+			])],
+			'sumasegurada': ['', Validators.compose([
+				Validators.required
+			])],
+			'clave': ['', Validators.compose([
+				Validators.required
+			])],
+			'cobertura': ['', Validators.compose([
+				Validators.required
+			])]
 		});
 	}
 }
