ac2984258230c84ded04e41dc50983b6f3734178
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-02-13 13:55:46 -0600

    Se agregan catálogos (constantes) de 'Formas de pago' y 'Paquetes de cobertura'

---
+++ front-end/code/PortalEmisionHIR/src/app/core/data/formas-pago.ts
@@ -0,0 +1,8 @@
+import { FormaPago } from 'app/core/models/forma-pago';
+
+export const FORMASPAGO: FormaPago[] = [
+	{ idFormaPago: 1, descFormaPago: 'Anual' },
+	{ idFormaPago: 2, descFormaPago: 'Semestral' },
+	{ idFormaPago: 3, descFormaPago: 'Trimestral' },
+	{ idFormaPago: 4, descFormaPago: 'Mensual' }
+];
