2ace674e341dba9d6ed56a63a5bc285c97044f8f
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-02-13 17:18:16 -0600

    Formato

--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/gastos-funerarios/gastos-funerarios.module.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/gastos-funerarios/gastos-funerarios.module.ts
@@ -9,8 +9,8 @@ import { GastosFunerariosP2Component }	 from './gastos-funerarios-p2.component';
 import { MyDatePickerModule }			 from 'mydatepicker';
 
 import { GastosFunerariosRoutingModule } from './gastos-funerarios-routing.module';
-import { ExtendFormControlModule }  	 from 'app/modulos/shared/extend-form-control/extend-form-control.module';
-import { PolicyHolderTableModule }  from 'app/modulos/shared/policyholder-table/policyholder-table.module';
+import { ExtendFormControlModule }		 from 'app/modulos/shared/extend-form-control/extend-form-control.module';
+import { PolicyHolderTableModule }		 from 'app/modulos/shared/policyholder-table/policyholder-table.module';
 import { ProgressControlModule }		 from 'app/modulos/shared/progress-control/progress-control.module';
 
 @NgModule({
