1d7f6ae1b707da901338ecfe7bdc252198325060
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-02-26 17:17:57 -0600

    Se agregan métodos de 'login' y 'logout' en el servicio de autenticación y se agrega el modelo para manejo de datos del formulario de login.

--- front-end/code/PortalEmisionHIR/src/app/core/services/authentication/authentication.service.ts
+++ front-end/code/PortalEmisionHIR/src/app/core/services/authentication/authentication.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable }	   from '@angular/core';
+import { Router }		   from '@angular/router';
+
+import { Usuario }		   from 'app/core/models/acceso/usuario';
+
+import { AppModalService } from 'app/modulos/shared/app-modal/app-modal.service';
 
 @Injectable()
 export class AuthenticationService {
 
-	constructor() {}
+	constructor(
+		private appModalService: AppModalService,
+		private router: Router
+	){}
+
+	public login( usuario: Usuario ): void {
+		if( usuario.nombre !== 'ppantera@hir.com' && usuario.clave !== 'ChocoMilk$2018' ) {
+			this.appModalService.openModal( 'error', 'Usuario o contraseña  incorrectos, intenta nuevamente' );
+		} else {
+			localStorage.setItem( 'token', 'fooSession' );
+			this.router.navigateByUrl( '/acceso/codigo' );
+		}
+	}
 
 	public isAuthenticated(): boolean {
 		const token = localStorage.getItem( 'token' );
@@ -12,7 +29,12 @@ export class AuthenticationService {
 			return true;
 		} else {
 			return false;
-		}    	
-  	}
+		}
+	}
+
+	public logout(): void {
+		localStorage.clear();
+		this.router.navigateByUrl( '/acceso/login' );
+	}
 
 }
