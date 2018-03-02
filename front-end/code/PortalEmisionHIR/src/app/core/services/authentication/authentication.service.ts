baf77cd23170f0db24e78462bed18f2d904a00bc
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-03-02 11:43:10 -0600

    Se agrega bandera en el servicio de autenticación para mostrar/ocultar contenido de sesión (nombre de usuario, menú, etc.).

--- front-end/code/PortalEmisionHIR/src/app/core/services/authentication/authentication.service.ts
+++ front-end/code/PortalEmisionHIR/src/app/core/services/authentication/authentication.service.ts
@@ -1,5 +1,7 @@
 import { Injectable }	   from '@angular/core';
 import { Router }		   from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable }	   from 'rxjs/Observable';
 
 import { Usuario }		   from 'app/core/models/acceso/usuario';
 
@@ -7,17 +9,23 @@ import { AppModalService } from 'app/core/components/app-modal/app-modal.service
 
 @Injectable()
 export class AuthenticationService {
+	private authenticated = new BehaviorSubject<boolean>( false );
 
 	constructor(
 		private appModalService: AppModalService,
 		private router: Router
 	){}
 
+	getAuthenticated(): Observable<boolean> {
+		return this.authenticated.asObservable();
+	}
+
 	public login( usuario: Usuario ): void {
 		if( usuario.nombre !== 'ppantera@hir.com' || usuario.clave !== 'ChocoMilk$2018' ) {
 			this.appModalService.openModal( 'error', 'Usuario o contraseña  incorrectos, intenta nuevamente' );
 		} else {
 			localStorage.setItem( 'token', 'fooSession' );
+			this.authenticated.next( true );
 			this.router.navigateByUrl( '/acceso/codigo' );
 		}
 	}
@@ -34,6 +42,7 @@ export class AuthenticationService {
 
 	public logout(): void {
 		localStorage.clear();
+		this.authenticated.next( false );
 		this.router.navigateByUrl( '/acceso/login' );
 		this.appModalService.openModal( 'success', 'Su sesión ha sido cerrada correctamente' );
 	}
--- front-end/code/PortalEmisionHIR/src/app/layout/app-header.component.ts
+++ front-end/code/PortalEmisionHIR/src/app/layout/app-header.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy }	 from '@angular/core';
+import { Subscription }			 from 'rxjs/Subscription';
+
+import { AuthenticationService } from 'app/core/services/authentication/authentication.service';
 
 @Component({
 	selector: 'pehir-header',
 	templateUrl: 'app-header.component.html'
 })
 
-export class AppHeaderComponent {}
+export class AppHeaderComponent implements OnDestroy {
+	private authenticated: boolean;
+	private subscription: Subscription;
+
+	constructor( private authenticationService: AuthenticationService ){
+		this.subscription = this.authenticationService
+								.getAuthenticated()
+								.subscribe( authenticated => this.authenticated = authenticated );
+	}
+
+	ngOnDestroy() {
+		this.subscription.unsubscribe();
+	}
+}
