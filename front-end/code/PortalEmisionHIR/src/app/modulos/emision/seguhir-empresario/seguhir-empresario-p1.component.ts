0a582041a4db09ec2bd34109902fe3b74dcf2cc0
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-03-15 10:38:56 -0600

    Modificaciones de estilos en los botones y validaciones de 'teléfono', 'celular' y 'correo' en la 'Emisión' de 'SeguHIR Empresario'.

--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/seguhir-empresario/seguhir-empresario-p1.component.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/seguhir-empresario/seguhir-empresario-p1.component.ts
@@ -5,8 +5,10 @@ import { Router }							  from '@angular/router';
 import { WSClientService }					  from 'app/core/services/ws-client.service';
 
 import { ApellidoValidator } 				  from 'app/core/validators/apellido.validator';
+import { CelularValidator }					  from 'app/core/validators/celular.validator';
 import { NombreValidator }					  from 'app/core/validators/nombre.validator';
 import { RfcValidator }						  from 'app/core/validators/rfc.validator';
+import { TelefonoValidator }				  from 'app/core/validators/telefono.validator';
 import { EntreEdadesValidator }				  from 'app/core/validators/entre-edades.validator';
 
 import { GENEROS }							  from 'app/core/data/generos';
@@ -104,16 +106,22 @@ export class SeguhirEmpresarioP1Component implements OnInit {
 				Validators.required
 			])],
 			'telefono': ['', Validators.compose([
-				Validators.required
+				Validators.required,
+				TelefonoValidator()
 			])],
 			'celular': ['', Validators.compose([
-				Validators.required
+				Validators.required,
+				CelularValidator()
 			])],
 			'correoe1': ['', Validators.compose([
-				Validators.required
+				Validators.required,
+				Validators.email,
+				Validators.maxLength(50)
 			])],
 			'correoe2': ['', Validators.compose([
-				Validators.required
+				Validators.required,
+				Validators.email,
+				Validators.maxLength(50)
 			])]
 		});
 	}
