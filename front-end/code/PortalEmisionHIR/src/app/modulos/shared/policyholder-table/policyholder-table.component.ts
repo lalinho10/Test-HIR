9460ac3e6554954084ed8792e3191a19644e773d
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-03-23 17:57:41 -0600

    Modificaciones para agregar funcionalidad de 'Edición de beneficiarios' en el componente de 'Tabla de beneficiarios'.

--- front-end/code/PortalEmisionHIR/src/app/modulos/shared/policyholder-table/policyholder-table.component.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/shared/policyholder-table/policyholder-table.component.ts
@@ -1,16 +1,16 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormGroup, FormBuilder, Validators }	   from '@angular/forms';
+import { Component, EventEmitter, OnInit, Output }		 from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { WSClientService }						   from 'app/core/services/ws-client.service';
+import { WSClientService }								 from 'app/core/services/ws-client.service';
 
-import { NombreValidator }						   from 'app/core/validators/nombre.validator';
-import { PorcentajeValidator }					   from 'app/core/validators/porcentaje.validator';
+import { NombreValidator }								 from 'app/core/validators/nombre.validator';
+import { PorcentajeValidator }							 from 'app/core/validators/porcentaje.validator';
 
-import { Beneficiario }							   from 'app/core/models/beneficiario';
+import { Beneficiario }									 from 'app/core/models/beneficiario';
 
-import { FECNACOPTIONS }						   from 'app/core/data/fecNacOptions';
+import { FECNACOPTIONS }								 from 'app/core/data/fecNacOptions';
 
-import { Parentesco }							   from 'app/core/models/parentesco';
+import { Parentesco }									 from 'app/core/models/parentesco';
 
 @Component({
 	selector: 'pehir-policyholder-table',
@@ -47,6 +47,13 @@ export class PolicyHolderTableComponent implements OnInit {
 		this.readCatalogs();
 
 		this.frmBeneficiario = this.fb.group({
+			'grupoAlta': this.crearGrupo(),
+			'gruposEdicion': this.fb.array( [] )
+		});
+	}
+
+	private crearGrupo(): FormGroup {
+		return this.fb.group({
 			'nombre': ['', Validators.compose([
 				Validators.required,
 				Validators.maxLength(50),
@@ -63,39 +70,112 @@ export class PolicyHolderTableComponent implements OnInit {
 				Validators.min(1),
 				Validators.max(100),
 				PorcentajeValidator()
-			])]
+			])],
+			'edicion': false
 		});
 	}
 
-	private eliminarBeneficiario( idUsuario: number ): void {
-		this.beneficiarios.splice( idUsuario, 1 );
+	private eliminarGrupoEdicion( idGrupo: number ): void {
+		let gruposEdicion: FormArray = this.frmBeneficiario.controls[ 'gruposEdicion' ] as FormArray;
+		gruposEdicion.removeAt( idGrupo );
+	}
+
+	private cancelarEdicion( idBeneficiario: number ): void {
+		let gruposEdicion: FormArray = this.frmBeneficiario.controls[ 'gruposEdicion' ] as FormArray;
+		let grupoEdicion = gruposEdicion.at( idBeneficiario ) as FormGroup;
+		grupoEdicion.reset();
+		grupoEdicion.controls[ 'edicion' ].setValue( false );
+	}
+
+	private modificarGrupoEdicion( idBeneficiario: number ): void {
+		let gruposEdicion: FormArray = this.frmBeneficiario.controls[ 'gruposEdicion' ] as FormArray;
+		let grupoEdicion = gruposEdicion.at( idBeneficiario ) as FormGroup;
+
+		if( grupoEdicion.valid ) {
+			this.modificarBeneficiario( idBeneficiario, grupoEdicion );
+			grupoEdicion.reset();
+			grupoEdicion.controls[ 'edicion' ].setValue( false );
+		} else {
+
+		}
+	}
+
+	private agregarGrupoEdicion(): void {
+		let gruposEdicion: FormArray = this.frmBeneficiario.controls[ 'gruposEdicion' ] as FormArray;
+		let grupoEdicion: FormGroup = this.crearGrupo();
+		gruposEdicion.push( grupoEdicion );
+	}
+
+	private eliminarBeneficiario( idBeneficiario: number ): void {
+		this.beneficiarios.splice( idBeneficiario, 1 );
+		this.validarSumatoriaPorcentajes();
+	}
+
+	private editarBeneficiario( idBeneficiario: number ): void {
+		let gruposEdicion: FormArray = this.frmBeneficiario.controls[ 'gruposEdicion' ] as FormArray;
+		let grupoEdicion = gruposEdicion.at( idBeneficiario ) as FormGroup;
+		let fechaCapturada = this.beneficiarios[ idBeneficiario ].fechaNacimiento;
+		let objetoFechaCal = {
+			date: {
+				year: fechaCapturada.getFullYear(),
+				month: fechaCapturada.getMonth() + 1,
+				day: fechaCapturada.getDate()
+			},
+			epoc: fechaCapturada.getTime() / 1000
+		};
+
+		grupoEdicion.controls[ 'nombre' ].setValue( this.beneficiarios[ idBeneficiario ].nombre );
+		grupoEdicion.controls[ 'fechanac' ].patchValue( objetoFechaCal );
+		grupoEdicion.controls[ 'parentesco' ].setValue( this.beneficiarios[ idBeneficiario ].idParentesco );
+		grupoEdicion.controls[ 'porcentajeSuma' ].setValue( this.beneficiarios[ idBeneficiario ].porcentajeSuma * 100 );
+		grupoEdicion.controls[ 'edicion' ].setValue( true );
+	}
+
+	private modificarBeneficiario( idBeneficiario: number, grupoEdicion: FormGroup ): void {
+		this.beneficiarios[ idBeneficiario ].nombre = grupoEdicion.get( 'nombre' ).value;
+		this.beneficiarios[ idBeneficiario ].fechaNacimiento = new Date( grupoEdicion.get( 'fechanac' ).value.epoc * 1000 );
+		this.beneficiarios[ idBeneficiario ].idParentesco = grupoEdicion.get( 'parentesco' ).value;
+
+		let parentesco = this.parentescos.filter( ( parentesco: Parentesco ) => parentesco.idParentesco == this.beneficiarios[ idBeneficiario ].idParentesco );
+
+		this.beneficiarios[ idBeneficiario ].descParentesco = parentesco[0].descParentesco;
+		this.beneficiarios[ idBeneficiario ].porcentajeSuma = grupoEdicion.get( 'porcentajeSuma' ).value / 100;
+
 		this.validarSumatoriaPorcentajes();
 	}
 
 	private agregarBeneficiario(): void {
 		let beneficiario: Beneficiario = new Beneficiario();
 
-		beneficiario.nombre = this.frmBeneficiario.controls[ 'nombre' ].value;
-		beneficiario.fechaNacimiento = new Date( this.frmBeneficiario.controls[ 'fechanac' ].value.epoc * 1000 );
-		beneficiario.idParentesco = this.frmBeneficiario.controls[ 'parentesco' ].value;
+		beneficiario.nombre = this.frmBeneficiario.get( 'grupoAlta.nombre' ).value;
+		beneficiario.fechaNacimiento = new Date( this.frmBeneficiario.get( 'grupoAlta.fechanac' ).value.epoc * 1000 );
+		beneficiario.idParentesco = this.frmBeneficiario.get( 'grupoAlta.parentesco' ).value;
 
 		let parentesco = this.parentescos.filter( ( parentesco: Parentesco ) => parentesco.idParentesco == beneficiario.idParentesco );
 
 		beneficiario.descParentesco = parentesco[0].descParentesco;
-		beneficiario.porcentajeSuma = this.frmBeneficiario.controls[ 'porcentajeSuma' ].value / 100;
+		beneficiario.porcentajeSuma = this.frmBeneficiario.get( 'grupoAlta.porcentajeSuma' ).value / 100;
 
 		this.beneficiarios.push( beneficiario );
 
 		this.validarSumatoriaPorcentajes();
 	}
 
-	private limpiarControles(): void {
-		this.frmBeneficiario.reset();
+	private eliminarRegistro( idRegistro: number ): void {
+		this.eliminarBeneficiario( idRegistro );
+		this.eliminarGrupoEdicion( idRegistro );
 	}
 
-	private mostrarErrores(): void {
-		Object.keys( this.frmBeneficiario.controls ).forEach( field => {
-			const control = this.frmBeneficiario.get( field );
+	private limpiarControlesAlta(): void {
+		let grupoAlta: FormGroup = this.frmBeneficiario.get( 'grupoAlta' ) as FormGroup;
+		grupoAlta.reset();
+	}
+
+	private mostrarErroresAlta(): void {
+		let grupoAlta: FormGroup = this.frmBeneficiario.get( 'grupoAlta' ) as FormGroup;
+
+		Object.keys( grupoAlta.controls ).forEach( field => {
+			const control = grupoAlta.get( field );
 			control.markAsDirty( { onlySelf: true } );
 		});
 	}
@@ -109,25 +189,28 @@ export class PolicyHolderTableComponent implements OnInit {
 
 		this.onValidateTable.emit( this.isValidSum );
 
-		if(!this.isValidSum) {
-			this.frmBeneficiario.controls[ 'nombre' ].enable();
-			this.frmBeneficiario.controls[ 'fechanac' ].enable();
-			this.frmBeneficiario.controls[ 'parentesco' ].enable();
-			this.frmBeneficiario.controls[ 'porcentajeSuma' ].enable();
+		if( !this.isValidSum ) {
+			this.frmBeneficiario.get( 'grupoAlta.nombre' ).enable();
+			this.frmBeneficiario.get( 'grupoAlta.fechanac' ).enable();
+			this.frmBeneficiario.get( 'grupoAlta.parentesco' ).enable();
+			this.frmBeneficiario.get( 'grupoAlta.porcentajeSuma' ).enable();
 		} else {
-			this.frmBeneficiario.controls[ 'nombre' ].disable();
-			this.frmBeneficiario.controls[ 'fechanac' ].disable();
-			this.frmBeneficiario.controls[ 'parentesco' ].disable();
-			this.frmBeneficiario.controls[ 'porcentajeSuma' ].disable();
+			this.frmBeneficiario.get( 'grupoAlta.nombre' ).disable();
+			this.frmBeneficiario.get( 'grupoAlta.fechanac' ).disable();
+			this.frmBeneficiario.get( 'grupoAlta.parentesco' ).disable();
+			this.frmBeneficiario.get( 'grupoAlta.porcentajeSuma' ).disable();
 		}
 	}
 
-	private validarFomulario(): void {
-		if( this.frmBeneficiario.valid ) {
+	private validarGrupoAlta(): void {
+		let grupoAlta: FormGroup = this.frmBeneficiario.get( 'grupoAlta' ) as FormGroup;
+
+		if( grupoAlta.valid ) {
 			this.agregarBeneficiario();
-			this.limpiarControles();
+			this.agregarGrupoEdicion();
+			this.limpiarControlesAlta();
 		} else {
-			this.mostrarErrores();
+			this.mostrarErroresAlta();
 		}
 	}
 }
