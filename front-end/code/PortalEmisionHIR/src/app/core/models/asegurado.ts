0ed0a9d66cba1183f52d8c85c3b4a45bba8132fd
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-03-27 12:10:55 -0600

    Se agrega el modelo 'Asegurado'.

---
+++ front-end/code/PortalEmisionHIR/src/app/core/models/asegurado.ts
@@ -0,0 +1,12 @@
+export class Asegurado {
+	nombre: string;
+	apaterno: string;
+	amaterno: string;
+	fechaNacimiento: Date;
+	idEstadoCivil: number;
+	descEstadoCivil: string;
+	idGenero: number;
+	descGenero: string;
+	idOcupacion: number;
+	descOcupacion: string;
+}
