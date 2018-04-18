c28b77af04fcdd47cd828c2d207ba62a7957cce9
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-04-18 16:07:37 -0500

    Se modifica el servicio de 'Cotización' para identificar el producto que se está cotizando, persistir los datos capturados en la cotización correspondiente, hacer la redirección a la cotización/emisión correspondiente al producto cotizado e identificar si al volver se hará edición de datos en la cotización o no.

--- front-end/code/PortalEmisionHIR/src/app/modulos/cotizacion/cotizacion.service.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/cotizacion/cotizacion.service.ts
@@ -1,28 +1,53 @@
-import { Injectable } from '@angular/core';
+import { Injectable }  from '@angular/core';
 
-import { PRODUCTOS }  from 'app/core/data/productos';
+import { Cotizacion } from './cotizacion';
 
-import { Producto }   from 'app/core/models/producto';
+import { PRODUCTOS }   from 'app/core/data/productos';
+
+import { Producto }    from 'app/core/models/producto';
 
 @Injectable()
 export class CotizacionService {
-	private idProducto: number;
+	private producto: Producto;
+
+	private cotizacion: Cotizacion;
+
+	private editarCotizacion: boolean = false;
 
 	private productos = PRODUCTOS;
 
 	definirProducto( idProducto: number ) {
-		this.idProducto = idProducto;
+		let filtroProductos = this.productos.filter( ( producto: Producto ) => producto.idProducto == idProducto );
+		this.producto = filtroProductos[ 0 ];
+	}
+
+	obtenerDescripcionProducto() {
+		return this.producto.descProducto;
+	}
+
+	definirCotizacion( cotizacion: Cotizacion ) {
+		this.cotizacion = cotizacion;
+	}
+
+	obtenerCotizacion(): Cotizacion {
+		return this.cotizacion;
+	}
+
+	definirEdicion( editarCotizacion: boolean ) {
+		this.editarCotizacion = editarCotizacion;
+	}
+
+	esEdicion() {
+		return this.editarCotizacion;
 	}
 
 	obtenerRutaRegreso(): string {
-		let producto: Producto[] = this.productos.filter( ( producto: Producto ) => producto.idProducto == this.idProducto );
-		let path: string = '/cotizacion/' + producto[ 0 ].pathProducto;
+		let path: string = '/cotizacion/' + this.producto.pathProducto;
 		return path;
 	}
 
 	obtenerRutaEmision(): string {
-		let producto: Producto[] = this.productos.filter( ( producto: Producto ) => producto.idProducto == this.idProducto );
-		let path: string = '/emision/' + producto[ 0 ].pathProducto;
+		let path: string = '/emision/' + this.producto.pathProducto;
 		return path;
 	}
 }
