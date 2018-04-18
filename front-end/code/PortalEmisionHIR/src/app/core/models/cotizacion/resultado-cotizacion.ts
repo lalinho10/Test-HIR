bc604db47d290e57c0f4ed4eb37968cb54d36227
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-04-18 16:03:08 -0500

    Se quita constructor para el modelo de 'ResultadoCotizacion'.

--- front-end/code/PortalEmisionHIR/src/app/core/models/cotizacion/resultado-cotizacion.ts
+++ front-end/code/PortalEmisionHIR/src/app/core/models/cotizacion/resultado-cotizacion.ts
@@ -7,15 +7,4 @@ export class ResultadoCotizacion {
 	sexo: string;
 	tipoPago: string;
 	montoPago: number;
-
-	constructor( resultadoCotizacion: ResultadoCotizacion ) {
-		this.sumaAsegurada = resultadoCotizacion.sumaAsegurada;
-		this.nombreUsuario = resultadoCotizacion.nombreUsuario;
-		this.correoUsuario = resultadoCotizacion.correoUsuario;
-		this.telefonoUsuario = resultadoCotizacion.telefonoUsuario;
-		this.edad = resultadoCotizacion.edad;
-		this.sexo = resultadoCotizacion.sexo;
-		this.tipoPago = resultadoCotizacion.tipoPago;
-		this.montoPago = resultadoCotizacion.montoPago;
-	}
 }
