27df1ec62a3912d4a48b694335268e69a63a313c
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-05-02 12:17:26 -0500

    Reorganización de carpetas y componentes en subtransacción y flujos de 'Emisión - Vida con ahorro' de acuerdo al estándar de Angular.

--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/vida-ahorro/vida-ahorro-routing.module.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/vida-ahorro/vida-ahorro-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule }					  from '@angular/core';
 import { RouterModule, Routes }		  from '@angular/router';
 
 import { VidaAhorroComponent }		  from './vida-ahorro.component';
-import { VidaAhorroP1Component }	  from './vida-ahorro-p1.component';
-import { VidaAhorroP2Component }	  from './vida-ahorro-p2.component';
-import { VidaAhorroP3Component }	  from './vida-ahorro-p3.component';
-import { VidaAhorroP4Component }	  from './vida-ahorro-p4.component';
-import { VidaAhorroP5Component }	  from './vida-ahorro-p5.component';
-import { VidaAhorroP6Component }	  from './vida-ahorro-p6.component';
+import { VidaAhorroP1Component }	  from './p1-usuario/vida-ahorro-p1.component';
+import { VidaAhorroP2Component }	  from './p2-seguro/vida-ahorro-p2.component';
+import { VidaAhorroP3Component }	  from './p3-medico/vida-ahorro-p3.component';
+import { VidaAhorroP4Component }	  from './p4-asegurados/vida-ahorro-p4.component';
+import { VidaAhorroP5Component }	  from './p5-beneficiarios/vida-ahorro-p5.component';
+import { VidaAhorroP6Component }	  from './p6-agentes/vida-ahorro-p6.component';
 
 import { AuthenticationGuardService } from 'app/core/services/authentication/authentication-guard.service';
 
--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/vida-ahorro/vida-ahorro.module.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/vida-ahorro/vida-ahorro.module.ts
@@ -3,12 +3,12 @@ import { CommonModule }				from '@angular/common';
 import { ReactiveFormsModule }		from '@angular/forms';
 
 import { VidaAhorroComponent }	 	from './vida-ahorro.component';
-import { VidaAhorroP1Component }	from './vida-ahorro-p1.component';
-import { VidaAhorroP2Component }	from './vida-ahorro-p2.component';
-import { VidaAhorroP3Component }	from './vida-ahorro-p3.component';
-import { VidaAhorroP4Component }	from './vida-ahorro-p4.component';
-import { VidaAhorroP5Component }	from './vida-ahorro-p5.component';
-import { VidaAhorroP6Component }	from './vida-ahorro-p6.component';
+import { VidaAhorroP1Component }	from './p1-usuario/vida-ahorro-p1.component';
+import { VidaAhorroP2Component }	from './p2-seguro/vida-ahorro-p2.component';
+import { VidaAhorroP3Component }	from './p3-medico/vida-ahorro-p3.component';
+import { VidaAhorroP4Component }	from './p4-asegurados/vida-ahorro-p4.component';
+import { VidaAhorroP5Component }	from './p5-beneficiarios/vida-ahorro-p5.component';
+import { VidaAhorroP6Component }	from './p6-agentes/vida-ahorro-p6.component';
 
 import { MyDatePickerModule }		from 'mydatepicker';
 
