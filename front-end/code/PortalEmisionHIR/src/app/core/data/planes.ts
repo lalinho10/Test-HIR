c90f9778b49811ec15c88e36d8af93a45a9baa16
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-05-15 13:53:49 -0500

    Se agregan datos y enum de 'Plan'.

---
+++ front-end/code/PortalEmisionHIR/src/app/core/data/planes.ts
@@ -0,0 +1,8 @@
+import { Plan } from 'app/core/models/plan';
+
+export const PLANES: Plan[] = [
+	{ idPlan: 1, descPlan: 'Individual' },
+	{ idPlan: 2, descPlan: 'Conyugal' },
+	{ idPlan: 3, descPlan: 'Hijos' },
+	{ idPlan: 4, descPlan: 'Familiar' }
+];
