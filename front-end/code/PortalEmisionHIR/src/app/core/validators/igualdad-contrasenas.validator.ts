98ab5b5cf33d5a7a39c9fca369172572fa50e1be
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-06-11 13:10:23 -0500

    Modificacion validación

--- front-end/code/PortalEmisionHIR/src/app/core/validators/igualdad-contrasenas.validator.ts
+++ front-end/code/PortalEmisionHIR/src/app/core/validators/igualdad-contrasenas.validator.ts
@@ -3,8 +3,8 @@ import { FormGroup, ValidatorFn } from '@angular/forms';
 export function IgualdadContrasenasValidator( contrasenaKey: string, confContrasenaKey: string ): ValidatorFn {
 	
 	return ( formGroup: FormGroup ): { [ key: string ]: any } => {
-		const valor1 = formGroup.controls[ contrasenaKey ].value;
-		const valor2 = formGroup.controls[ confContrasenaKey ].value;
+		const valor1 = formGroup.get( contrasenaKey ).value;
+		const valor2 = formGroup.get( confContrasenaKey ).value;
 
 		if( valor1 !== valor2 ) {
 			return { 'igualdadContrasenas': true }
