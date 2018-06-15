34712a5725b24454eb23a44a6f571b530bcb5e08
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-06-15 13:34:59 -0500

    Se agregan el modelo de datos para la descripción del producto 'A . P. Individual'

---
+++ front-end/code/PortalEmisionHIR/src/app/core/data/productos/apindividual.ts
@@ -0,0 +1,23 @@
+import { DetalleProducto } from './detalle-producto';
+
+let detalleApindividual: DetalleProducto = new DetalleProducto();
+
+	detalleApindividual.descProducto = 'Un reembolso por  gastos médicos que origine el accidente. Una indemnización en caso de fallecimiento por el accidente.';
+	detalleApindividual.beneficios =
+	[
+		'Una cobertura por 365 días, las 24 horas del día.',
+		'Pago de indemnizaciones en ventanillas bancarias.',
+		'Garantia de pago de reembolsos en 7 días.'
+	],
+	detalleApindividual.ventajas =
+	[
+		'Deducible mínimo de $500 pesos.',
+		'Plan de protección anual.',
+		'Renovación automática.',
+		'Tarifa única.',
+		'Formato de pago: efectivo, cheque, tarjeta de crédito o débito.'
+	],
+	detalleApindividual.rutaImagen1 = './assets/img/sinImagen.png';
+	detalleApindividual.rutaImagen2 = './assets/img/sinImagen.png';
+
+export const DET_PROD_API = detalleApindividual;
--- front-end/code/PortalEmisionHIR/src/app/modulos/productos/apindividual/apindividual.component.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/productos/apindividual/apindividual.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { DetalleProducto } from 'app/core/data/productos/detalle-producto';
+import { DET_PROD_API }	   from 'app/core/data/productos/apindividual';
+
 @Component({
 	selector: 'pehir-papindividual',
 	templateUrl: 'apindividual.component.html'
@@ -7,4 +10,6 @@ import { Component } from '@angular/core';
 
 export class ApindividualComponent {
 	titulo: string = 'Productos - Accidentes Personales Individual';
+
+	detalleApindividual: DetalleProducto = DET_PROD_API;
 }
