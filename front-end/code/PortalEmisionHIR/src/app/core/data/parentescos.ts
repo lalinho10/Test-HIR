160af101bc788842042c3a394dee09a9f92b6bee
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-07-19 12:24:39 -0500

    Reingeniería en constante de datos del modelo 'Parentesco'

--- front-end/code/PortalEmisionHIR/src/app/core/data/parentescos.ts
+++ front-end/code/PortalEmisionHIR/src/app/core/data/parentescos.ts
@@ -1,16 +1,16 @@
 import { Parentesco } from 'app/core/models/parentesco';
 
 export const PARENTESCOS: Parentesco[] = [
-	{ idParentesco: 1078, descParentesco: 'Hermano(a)' },
-	{ idParentesco: 1079, descParentesco: 'Hijo(a)' },
-	{ idParentesco: 1117, descParentesco: 'Esposo(a)' },
-	{ idParentesco: 1118, descParentesco: 'Concubino(a)' },
-	{ idParentesco: 1119, descParentesco: 'Madre' },
-	{ idParentesco: 1120, descParentesco: 'Padre' },
-	{ idParentesco: 1121, descParentesco: 'Sobrino(a)' },
-	{ idParentesco: 2186, descParentesco: 'Tío(a)' },
-	{ idParentesco: 2187, descParentesco: 'Otro' },
-	{ idParentesco: 2188, descParentesco: 'Ninguno' },
-	{ idParentesco: 2728, descParentesco: 'Preferente' },
-	{ idParentesco: 6926, descParentesco: 'Nieto(a)' }
+	{ id: 1078, desc: 'Hermano(a)' },
+	{ id: 1079, desc: 'Hijo(a)' },
+	{ id: 1117, desc: 'Esposo(a)' },
+	{ id: 1118, desc: 'Concubino(a)' },
+	{ id: 1119, desc: 'Madre' },
+	{ id: 1120, desc: 'Padre' },
+	{ id: 1121, desc: 'Sobrino(a)' },
+	{ id: 2186, desc: 'Tío(a)' },
+	{ id: 2187, desc: 'Otro' },
+	{ id: 2188, desc: 'Ninguno' },
+	{ id: 2728, desc: 'Preferente' },
+	{ id: 6926, desc: 'Nieto(a)' }
 ];
