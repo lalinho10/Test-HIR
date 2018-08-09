0ff9039e170ff99b9b88d2dda8dac6cd854f3f85
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-08-09 17:34:13 -0500

    Se quitan los campos de telefono en 'Emisión - A. P. Individual'

--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/apindividual/p1-usuario/apindividual-p1.component.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/apindividual/p1-usuario/apindividual-p1.component.ts
@@ -20,9 +20,7 @@ import { CelularValidator }					  from 'app/core/validators/celular.validator';
 import { CodigoPostalValidator }			  from 'app/core/validators/codigo-postal.validator';
 import { NombreValidator }					  from 'app/core/validators/nombre.validator';
 import { RfcValidator }						  from 'app/core/validators/rfc.validator';
-import { TelefonoValidator }				  from 'app/core/validators/telefono.validator';
 import { EntreEdadesValidator }				  from 'app/core/validators/entre-edades.validator';
-import { DiferenciaTelefonosValidator }		  from 'app/core/validators/diferencia-telefonos.validator';
 
 @Component({
 	selector: 'pehir-apindividual-p1',
@@ -119,21 +117,6 @@ export class ApindividualP1Component implements OnInit {
 				Validators.required,
 				CodigoPostalValidator()
 			])],
-
-			'telefonos': this.fb.group({
-				'telefono': ['', Validators.compose([
-					Validators.required,
-					TelefonoValidator()
-				])],
-				'celular': ['', Validators.compose([
-					Validators.required,
-					CelularValidator()
-				])]
-			},
-			{
-				validator: DiferenciaTelefonosValidator( 'telefono', 'celular' )
-			}),
-
 			'gobierno': ['', Validators.compose([
 				Validators.required
 			])],
@@ -186,8 +169,6 @@ export class ApindividualP1Component implements OnInit {
 		this.frmApindividualP1.get( 'calleNumero' ).setValue( this.apindividualP1Service.getModelP1().calleNumero );
 		this.frmApindividualP1.get( 'cp' ).setValue( this.apindividualP1Service.getModelP1().cp );
 		this.frmApindividualP1.get( 'coloniaPoblacion' ).setValue( this.apindividualP1Service.getModelP1().coloniaPoblacion );
-		this.frmApindividualP1.get( 'telefonos.telefono' ).setValue( this.apindividualP1Service.getModelP1().telefono );
-		this.frmApindividualP1.get( 'telefonos.celular' ).setValue( this.apindividualP1Service.getModelP1().celular );
 		this.frmApindividualP1.get( 'gobierno' ).setValue( this.apindividualP1Service.getModelP1().gobierno );
 		this.frmApindividualP1.get( 'especifiqueGob' ).setValue( this.apindividualP1Service.getModelP1().especifiqueGob );
 		this.frmApindividualP1.get( 'parienteGob' ).setValue( this.apindividualP1Service.getModelP1().parienteGob );
--- front-end/code/PortalEmisionHIR/src/app/modulos/emision/apindividual/p1-usuario/apindividual-p1.service.ts
+++ front-end/code/PortalEmisionHIR/src/app/modulos/emision/apindividual/p1-usuario/apindividual-p1.service.ts
@@ -23,8 +23,6 @@ export class ApindividualP1Service {
 		this.apindividualP1.estado = frmApindividualP1.estado;
 		this.apindividualP1.delegacionMunicipio = frmApindividualP1.delegacionMunicipio;
 		this.apindividualP1.coloniaPoblacion = frmApindividualP1.coloniaPoblacion;
-		this.apindividualP1.telefono = frmApindividualP1.telefonos.telefono;
-		this.apindividualP1.celular = frmApindividualP1.telefonos.celular;
 		this.apindividualP1.gobierno = frmApindividualP1.gobierno;
 		this.apindividualP1.especifiqueGob = ( frmApindividualP1.gobierno ) ? frmApindividualP1.especifiqueGob : undefined;
 		this.apindividualP1.parienteGob = frmApindividualP1.parienteGob;
