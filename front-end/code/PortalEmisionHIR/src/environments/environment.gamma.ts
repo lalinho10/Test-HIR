55bcde5621e118be97699fb730ce2b9c9b00be23
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-08-23 12:30:52 -0500

    Se modifica el dominio del host de la API REST y la llave pública del recaptcha en el archivo de ambiente gamma

--- front-end/code/PortalEmisionHIR/src/environments/environment.gamma.ts
+++ front-end/code/PortalEmisionHIR/src/environments/environment.gamma.ts
@@ -4,11 +4,11 @@
 // archivo (environment.gamma.ts) en vez del default (environment.ts)
 
 export const environment = {
-	appHost: 'http://177.241.244.250:8080/service-rest/dtiServices',
+	appHost: 'http://emision.hirseguros.mx:8080/service-rest/dtiServices',
 	environmentName: 'Gamma',
 	development: false,
 	production: false,
-	recaptchaSiteKey: '6LfG6V0UAAAAAAnADp0QFhJzKeG2CIYzRQ0jzKqj',
+	recaptchaSiteKey: '6Lc40GsUAAAAAJrpSbQgYACCsXNq2i7k-ajkcIzc',
 	openpayMerchantId: 'mp2lfbdg2vryzcqnqmfm',
 	openpayPublicApiKey: 'pk_c1c7ea14443945e4b361164b4e43cc85',
 	openpaySandboxMode: true
