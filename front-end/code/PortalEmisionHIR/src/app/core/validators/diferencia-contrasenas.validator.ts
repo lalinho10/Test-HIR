74e47920e71873e2a7a4501d8b7e4332b3d8fb9c
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-11-28 17:30:22 -0600

    Corrección

--- front-end/code/PortalEmisionHIR/src/app/core/validators/diferencia-contrasenas.validator.ts
+++ front-end/code/PortalEmisionHIR/src/app/core/validators/diferencia-contrasenas.validator.ts
@@ -5,7 +5,7 @@ export function DiferenciaContrasenasValidator( contrasena1Key: string, contrase
 	return ( formGroup: FormGroup ): { [ key: string ]: any } => {
 		const valor1 = formGroup.get( contrasena1Key ).value;
 		const valor2 = formGroup.get( contrasena2Key ).value;
-console.log( 'Valor 1: ' + valor1 + ', valor 2: ' + valor2 );
+
 		if( valor1 === valor2 ) {
 			return { 'diferenciaContrasenas': true }
 		}
