4fdfb355bf44ae78e68bc4aae515cf04664647b9
Eduardo Jimenez Alvarez <eduardo.jimenez@planetmedia.com.mx>
2018-12-03 17:50:34 -0600

    Se crea el componente de carrusel

---
+++ front-end/code/PortalEmisionHIR/src/app/modulos/shared/carousel/carousel.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'pehir-carousel',
+	templateUrl: 'carousel.component.html',
+	styleUrls: [ 'carousel.component.css' ]
+})
+
+export class CarouselComponent implements OnInit {
+	@Input() carouselImages: string[];
+
+	selImage: number = 1;
+
+	ngOnInit() {
+		//setInterval( () => this.fnMove( 1 ), 2000 );
+	}
+
+	fnMove( i: number ): void {
+		this.selImage = this.selImage + i;
+
+		if( this.selImage > this.carouselImages.length ) {
+			this.selImage = 1;
+		}
+		if( this.selImage < 1 ) {
+			this.selImage = this.carouselImages.length;
+		}
+	}
+}
